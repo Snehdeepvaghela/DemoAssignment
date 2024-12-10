a9e53a8c0a3c1b8b1a06685ad7e192f0f8ef487d
Snehdeepsinh <snehdeepsinh.mca12@gmail.com>
2024-12-10 12:13:51 +0530

    Create LoginPage_PGfactory.java

---
+++ TestDemoProject/src/test/java/Pages/LoginPage_PGfactory.java
@@ -0,0 +1,59 @@
+package Pages;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import TestData.LoginData;
+
+public class LoginPage_PGfactory {
+
+	@FindBy(id = "email")
+	WebElement Usernamebox;
+	@FindBy(id = "password")
+	WebElement Passwordbox;
+	@FindBy(xpath = "//*[@id='button-backtologin' and @class = 'e-btn e-tertiary login-button width-login-btn']")
+	WebElement LoginButton;
+	@FindBy(id = "next")
+	WebElement SubmiButton;
+	@FindBy(xpath = "//*[@aria-label=\"Electoral registration\" and @class=\"submenuFocus dropdown\"]")
+	WebElement Elector;
+	LoginData Data = new LoginData();
+	////*[@aria-label="Electoral registration" and @class="submenuFocus dropdown"]
+	
+	public LoginPage_PGfactory(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+
+	}
+
+	public void buttonclick(WebElement element, WebDriver driver) {
+		element.click();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+	}
+
+	public void enterValue(WebElement element, String Value) {
+		element.sendKeys(Value);
+
+	}
+	
+	/*
+	 * WebDriverWait wait = new WebDriverWait(driver, 40);
+	 * wait.until(ExpectedConditions.elementToBeClickable(By.id("urelementid")));
+	 */
+
+	
+	public void loginWithValidCred(WebDriver driver) {
+		driver.get(Data.getLoginURL());
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		buttonclick(LoginButton , driver);
+		enterValue(Usernamebox, Data.getUserName());
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		enterValue(Passwordbox, Data.getPassword());
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		buttonclick(SubmiButton, driver);
+		
+	}
+
+}
